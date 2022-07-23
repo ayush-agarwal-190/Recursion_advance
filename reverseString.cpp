50536c30c63d049a2fc720b2deb663c6d84ee31a
Ayush Agarwal <95439195+ayush-agarwal-190@users.noreply.github.com>
2022-07-23 17:48:06 +0530

    Add files via upload

---
+++ reverseString.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void reverse(string s)
+{
+    if(s.length() == 0)
+    {
+        return;
+    }
+     
+    string ros = s.substr(1);
+    reverse(ros);
+    cout<<s[0];
+}
+
+int main()
+{
+  
+  reverse("Ayush");
+
+    return 0;
+}
